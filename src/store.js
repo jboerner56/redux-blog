5e180ce20565e3568c0fef7b7a44646a7681a515
Jason Boerner <jboerner56@gamil.com>
2019-05-09 09:59:32 -0400

    store, rootReducer and rredux dev tools

--- src/store.js
+++ src/store.js
@@ -11,6 +11,9 @@ const rootReducer = combineReducers({
     posts: posts
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
 export default store;
 
