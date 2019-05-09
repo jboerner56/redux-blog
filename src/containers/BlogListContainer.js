87ef2eede137d7a148ebecf031843197b2f27e7f
Jason Boerner <jboerner56@gamil.com>
2019-05-09 11:53:38 -0400

    adds components

---
+++ src/containers/BlogListContainer.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import BlogList from '../components/BlogList';
+// translate redux state into react props
+const mapStateToProps = (state) => {
+    return {
+        // react-prop: redux-state
+        posts: state.posts
+    }
+};
+// translate redux action-dispatch into react props
+
+
+
+
+const makeBlogListSmart = connect(mapStateToProps);
+const SmartBlogList = makeBlogListSmart(BlogList);
+
+export default SmartBlogList;
