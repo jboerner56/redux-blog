d4326919f2c0b14608de2dfca15b13161324b18b
Jason Boerner <jboerner56@gamil.com>
2019-05-09 11:57:46 -0400

    change BlogList so it shows the id and title

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 export default function BlogList({posts}) {
     // function implicitly returns <li>
-    const listItems = Object.values(posts).map(p =>  <li>{p.title}</li>)
+    // const listItems = Object.values(posts).map(p =>  <li>{p.title}</li>)
+
+    // if we want the id's as well as the blog posts
+    // get an array of the id's (using Object.keys)
+    // then map over the array
+    // with each id, we can access the blog post in the posts object
+    const listItems = Object.keys(posts).map(id => {
+        const theBlogPost = posts[id]
+        return <li>{theBlogPost} - {id}</li>
+    });
     return (
         <ol>
             {listItems}
