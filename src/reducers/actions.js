690f896c168cb225fd1d306ad91eab0dba6828a7
Jason Boerner <jboerner56@gamil.com>
2019-05-09 09:40:39 -0400

    define createPost action creator

---
+++ src/reducers/actions.js
@@ -0,0 +1,28 @@
+// actions
+
+export const ACTION_CREATE_POST = 'ACTION_CREATE_POST';
+
+export const ACTION_UPDATE_POST = 'ACTION_UPDATE_POST';
+
+export const ACTION_DELETE_POST = 'ACTION_DELETE_POST';
+
+// actin creators
+
+export function createPost(payload) {
+    // assumes the payload argument looks like this
+    // {
+        // title: 'some title',
+        // content: 'some content'
+    // }
+    return {
+        type: ACTION_CREATE_POST,
+        payload
+    }
+}
+export function updatePost() {
+
+}
+
+export function deletePost() {
+
+}
